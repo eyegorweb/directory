d07f5dd85c2671169dd157a2a409a56dd2213f38
Eduardo San Martin Morote <posva13@gmail.com>
2018-10-23 11:43:37 +0200

    update locally first, revert if wrong

--- src/App.vue
+++ src/App.vue
@@ -36,12 +36,15 @@ export default {
 
   methods: {
     async updateContact(contact) {
-      const updatedContact = await patchContact(contact);
-      this.contacts.splice(
-        this.contacts.findIndex(c => c.id === updatedContact.id),
-        1,
-        updatedContact
-      );
+      const index = this.contacts.findIndex(c => c.id === contact.id);
+      const oldContact = this.contacts[index];
+      this.contacts.splice(index, 1, contact);
+      try {
+        await patchContact(contact);
+      } catch (e) {
+        // revert
+        this.contacts.splice(index, 1, oldContact);
+      }
     }
   },
 
