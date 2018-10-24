4590ba147eb91da4f9e7012a9bb703e1577d052d
Eduardo San Martin Morote <posva13@gmail.com>
2018-10-24 17:34:18 +0200

    add store router sync

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { sync } from "vuex-router-sync";
+
+sync(store, router);
 
 Vue.use(BootstrapVue);
 
